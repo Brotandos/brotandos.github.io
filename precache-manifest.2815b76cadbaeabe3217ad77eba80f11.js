63727ddc78c211bc32be9145ddf9070a9682fa47
Brotandos <brotandos@gmail.com>
2019-11-06 22:22:55 +0600

    v0.0.D

---
+++ precache-manifest.2815b76cadbaeabe3217ad77eba80f11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a37179e8ae850cec7df6",
+    "url": "/css/app.14319eaa.css"
+  },
+  {
+    "revision": "a37179e8ae850cec7df6",
+    "url": "/js/app.1634d102.js"
+  },
+  {
+    "revision": "d9ca01b211f8a888d9aa",
+    "url": "/css/chunk-vendors.342ba411.css"
+  },
+  {
+    "revision": "d9ca01b211f8a888d9aa",
+    "url": "/js/chunk-vendors.a2ca87de.js"
+  },
+  {
+    "revision": "7b9b847037384fd38349e842496541ce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
